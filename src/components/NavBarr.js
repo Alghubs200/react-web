bec2291ae4dfcfb85de6252341aef034fdbb92a6
Alghubs <alghubs@outlook.com>
2024-10-21 12:35:29 +0530

    cc

--- src/components/NavBarr.js
+++ src/components/NavBarr.js
@@ -2,11 +2,27 @@ import { Link } from "react-router-dom";
 import "./NavBarr.css";
 import ReactLogo from "..//images/logo-react.png"
 import { Nav, Navbar, NavDropdown, Image, Container, Row, Col } from 'react-bootstrap';
+import React, {useState, useEffect} from 'react';
 
+export default function Navbarr() {
+  const [stickyClass, setStickyClass] = useState('');
 
+  useEffect(() => {
+    window.addEventListener('scroll', stickNavbar);
+    return () => window.removeEventListener('scroll', stickNavbar);
+  }, []);
 
-function NavBarr() {
-  return (
+  const stickNavbar = () => {
+    if (window !== undefined) {
+      let windowHeight = window.scrollY;
+      // window height changed for the demo
+      windowHeight > 5 ? setStickyClass('sticky-nav') : setStickyClass('');
+    }
+  };
+
+  return <div className={`navbar ${stickyClass}`}>
+    
+
     <Navbar expand="lg" className="main-navbar">
       <Container className="Cont-Class">
 
@@ -17,9 +33,8 @@ function NavBarr() {
               <Col>
                 <Image src={ReactLogo} fluid className="" />
               </Col>
-              <Col>
+              <Col className="brand-navbr">
                 <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-
               </Col>
 
 
@@ -69,9 +84,116 @@ function NavBarr() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
+
+
+    
+  </div>;
 }
 
-export default NavBarr;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function NavBarr() {
+
+//   return (
+
+//     <Navbar expand="lg" className="main-navbar">
+//       <Container className="Cont-Class">
+
+//         <Link to='/home' className="txt-dcr logo">
+
+//           <Container>
+//             <Row className="logo-row">
+//               <Col>
+//                 <Image src={ReactLogo} fluid className="" />
+//               </Col>
+//               <Col className="brand-navbr">
+//                 <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+//               </Col>
+
+
+
+//             </Row>
+
+//           </Container>
+
+
+//         </Link>
+
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="me-auto ">
+
+
+//             <Link to='/home' className="txt-dcr">
+//               <Nav.Link href="#home" className="">Home</Nav.Link>
+
+//             </Link>
+
+//             <Link to='/contact' className="txt-dcr">
+//               <Nav.Link href="#contact">Contact</Nav.Link>
+//             </Link>
+
+//             <Link to='/career' className="txt-dcr">
+//               <Nav.Link href="#career">Career</Nav.Link>
+
+//             </Link>
+
+//             <Link to='/about' className="txt-dcr">
+//               <Nav.Link href="#about">About</Nav.Link>
+
+//             </Link>
+//             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+//               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//               <NavDropdown.Item href="#action/3.2">
+//                 Another action
+//               </NavDropdown.Item>
+//               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//               <NavDropdown.Divider />
+//               <NavDropdown.Item href="#action/3.4">
+//                 Separated link
+//               </NavDropdown.Item>
+//             </NavDropdown>
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// }
+
+// export default NavBarr;
+
+
 
 
