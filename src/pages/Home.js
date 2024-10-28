befe1338df5894020002e9e4396fc38af97f90d2
Alghubs <alghubs@outlook.com>
2024-10-28 11:56:00 +0530

    ,

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import BGIMG from "..//images/bgm3.jpg"
 import diamond from "..//images/diamond.gif";
 import { Image, Container, Row, Col } from 'react-bootstrap';
 import 'animate.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
   return (
@@ -32,25 +34,55 @@ const Home = () => {
 
       </section>
       <section className='core-section'>
-      {/* <Image src={BGIMG} fluid className="bgimg1" /> */}
+        {/* <Image src={BGIMG} fluid className="bgimg1" /> */}
 
         <Container>
           <Row className='core-services'>
             <Col xs={12} md={9} className="core-col">
-            <h1 className="core-cont">
-              Core <span className="services-span">Services</span>
-            </h1>
+              <h1 className="core-cont">
+                Core <span className="services-span">Services</span>
+              </h1>
             </Col>
             <Col xs={12} md={3} className="">
-             <div className="book-consult">
-              <p>
-                Book Free Consultation
-              </p>
-             </div>
+              <div className="book-consult">
+                <p>
+                  Book Free Consultation
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+        <Container>
+          <p className="core-para">We’re all about bringing your ideas to life through innovative, tailor-made digital solutions. From sleek websites to engaging mobile apps and effective digital marketing strategies, we have everything you need to grow in today’s fast-paced digital world. Our experts don’t just build for today—we build with tomorrow in mind. Looking for design, development, or marketing solutions? Capecom is your partner for success. Ready to create something amazing?</p>
+        </Container>
+        <Container fluid className="box-contaienr">
+          <Row className='mt-5'>
+            <Col xs={12} md={3} className="corebox1 ">
+              <div className="corebox">
+                <FontAwesomeIcon icon={faGlobe} className="globe m-2" size="3x"/>
+
+              </div>
+            </Col>
+            <Col xs={12} md={3} className="corebox2 ">
+              <div className="corebox">
+              </div>
+            </Col>
+            <Col xs={12} md={3} className="corebox3 ">
+              <div className="corebox">
+              </div>
+            </Col>
+            <Col xs={12} md={3} className="corebox4 ">
+              <div className="corebox">
+              </div>
             </Col>
           </Row>
         </Container>
       </section>
+      <Container className="blank">
+      
+      </Container>
+
+
     </div>
 
   );
